f045bbbff33c45ef45e2028366cc796b902572b7
Merlan <merlan01tel@gmail.com>
2020-02-26 23:06:12 +0600

    Lab

---
+++ w4/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { ProductListComponent } from './product-list/product-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    ProductListComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: ProductListComponent },
+    ])
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ w4/src/app/products.ts
@@ -0,0 +1,102 @@
+export const products = [
+  {
+  name: 'Male Shoulder Bag',
+  id: 1,
+  // tslint:disable-next-line:max-line-length
+  img: 'https://ae01.alicdn.com/kf/HTB1kPZvaorrK1RkSne1q6ArVVXan/Male-Shoulder-Bags-USB-Charging-Crossbody-Bags-Men-Anti-Theft-Chest-Bag-School-Summer-Short-Trip.jpg',
+  rating: '4/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://www.aliexpress.com/item/32859112398.html?spm=a2g01.12617084.fdpcl001.1.185eLksPLksPgQ&gps-id=5547572&scm=1007.19201.130907.0&scm_id=1007.19201.130907.0&scm-url=1007.19201.130907.0&pvid=eff2b1bd-3f89-4d35-a9e4-e0181d479518',
+  // tslint:disable-next-line:max-line-length
+  description: 'Male Shoulder Bags USB Charging Crossbody Bags Men Anti Theft Chest Bag School Summer Short Trip Messengers Bag 2019 New Arrival'
+},
+{
+  name: 'Leather Bracelets',
+    id: 2,
+  // tslint:disable-next-line:max-line-length
+  img: 'https://ae01.alicdn.com/kf/H54575feea5184e40812499e4f010370cG/Vnox-4Pcs-Set-Braided-Wrap-Leather-Bracelets-for-Men-Vintage-Life-Tree-Rudder-Charm-Wood-Beads.jpg',
+  rating: '5/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://www.aliexpress.com/item/4000311346574.html?spm=a2g01.12617084.fdpcl001.3.185eLksPLksPgQ&gps-id=5547572&scm=1007.19201.130907.0&scm_id=1007.19201.130907.0&scm-url=1007.19201.130907.0&pvid=eff2b1bd-3f89-4d35-a9e4-e0181d479518',
+  // tslint:disable-next-line:max-line-length
+  description: 'Vnox 4Pcs/ Set Braided Wrap Leather Bracelets for Men Vintage Life Tree Rudder Charm Wood Beads Ethnic Tribal Wristbands',
+},
+{
+  name: 'Power Bank',
+    id: 3,
+  // tslint:disable-next-line:max-line-length
+  img: 'https://ae01.alicdn.com/kf/H70f0aaf582c6412cae6a84d77c030913i/Essager-10000mAh-Power-Bank-Slim-USB-10000-mAh-Powerbank-Portable-External-Battery-Charger-Pack-For-Xiaomi.jpg',
+  rating: '4.5/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://www.aliexpress.com/item/4000382846051.html?spm=a2g01.12617084.fdpcl001.11.185eLksPLksPgQ&gps-id=5547572&scm=1007.19201.130907.0&scm_id=1007.19201.130907.0&scm-url=1007.19201.130907.0&pvid=eff2b1bd-3f89-4d35-a9e4-e0181d479518',
+  // tslint:disable-next-line:max-line-length
+  description: 'Essager 10000mAh Power Bank Slim USB 10000 mAh Powerbank Portable External Battery Charger Pack For Xiaomi Mi 3 iPhone PoverBank'
+},
+{
+  name: 'Mask',
+    id: 4,
+  img: 'https://ae01.alicdn.com/kf/HTB1O0DLX.LrK1Rjy0Fjq6zYXFXa5/50-3.jpg',
+  rating: '4.9/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/32960387487.html?spm=a2g0o.home.15002.4.bbea1b86TzfqPL&gps-id=pcJustForYou&scm=1007.13562.158406.0&scm_id=1007.13562.158406.0&scm-url=1007.13562.158406.0&pvid=894d2bbf-15c8-48aa-8837-c16a103309cf&_t=gps-id:pcJustForYou,scm-url:1007.13562.158406.0,pvid:894d2bbf-15c8-48aa-8837-c16a103309cf,tpp_buckets:668%230%23131923%2318_668%23808%234093%2393_668%23888%233325%233',
+  description: '50 pcs. disposable face mask Dental anti-dust surgical face mask medical bacteria pollution of ships in March 2020'
+},
+{
+  name: 'Leather gloves',
+    id: 5,
+  img: 'https://ae01.alicdn.com/kf/Hf58a9df4ad634f1eb7bbfcbed739644cH/suotf.jpg',
+  rating: '2/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/33031031422.html?spm=a2g01.12597576.p99adbb.39.6985124agzxKYO&gps-id=7317346&scm=1007.19881.118560.0&scm_id=1007.19881.118560.0&scm-url=1007.19881.118560.0&pvid=3a490440-837e-4f44-a1a4-27b6d7ea02b8',
+  // tslint:disable-next-line:max-line-length
+  description: 'Sports shorts black suotf Fights MMA Boxing sports leather gloves Tiger Muay Thai fights boxing MMA gloves boxing Sanda Boxing gloves pads MMA'
+},
+{
+  name: 'Sleeve for bow',
+    id: 6,
+  img: 'https://ae01.alicdn.com/kf/HTB1MtfyX5rxK1RkHFCcq6AQCVXaA/-.jpg',
+  rating: '4.5/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/32957464814.html?spm=a2g0o.detail.1000060.3.4cd56325SeKe9Q&gps-id=pcDetailBottomMoreThisSeller&scm=1007.14977.145068.0&scm_id=1007.14977.145068.0&scm-url=1007.14977.145068.0&pvid=d8b4e75b-d255-44b6-8a83-566ce69cb403&_t=gps-id:pcDetailBottomMoreThisSeller,scm-url:1007.14977.145068.0,pvid:d8b4e75b-d255-44b6-8a83-566ce69cb403,tpp_buckets:668%230%23131923%2318_668%23808%234093%2393_668%23888%233325%233',
+  // tslint:disable-next-line:max-line-length
+  description: 'Cow Leather Protective Arm Archery Recurve Bow Longbow Traditional American Hunting Back Arm Protection Safe Protector'
+},
+{
+  name: 'Metal claws',
+    id: 7,
+  img: 'https://ae01.alicdn.com/kf/HTB1hFSSOXXXXXX5aXXXq6xXFXXXr/1.jpg',
+  rating: '3.5/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/32763586606.html?spm=a2g01.12597576.p99adbb.68.1722124aPigRaX&gps-id=7317346&scm=1007.19881.118560.0&scm_id=1007.19881.118560.0&scm-url=1007.19881.118560.0&pvid=ef1b9792-8601-446d-8deb-99b5f5dd949c',
+  description: '1 PC. metal claws Wolverines wolf claws high quality sophistication cosplay props Halloween for lovers + necklace'
+},
+{
+  name: 'Ring',
+    id: 8,
+  img: 'https://ae01.alicdn.com/kf/HTB1IHAbX1bviK0jSZFNq6yApXXa8/Vnox-8.jpg',
+  rating: '3.9/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/32885333554.html?spm=a2g01.12597576.p99adbb.48.1722124aPigRaX&gps-id=7315605&scm=1007.19881.118560.0&scm_id=1007.19881.118560.0&scm-url=1007.19881.118560.0&pvid=b5bfa42f-2292-4789-9970-246ff1d32e97',
+  description: 'Vnox 8mm Rotating Main Ring for Men Black Stainless Steel Casual Mens Anel Stylish Punk Spinner Alliance Jewelry'
+},
+{
+  name: 'cargo pants',
+    id: 9,
+  img: 'https://ae01.alicdn.com/kf/H9cad8ed531c04e8badd537b90edc58a98/-.jpg',
+  rating: '4/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/4000142581828.html?spm=a2g01.12597576.p99adbb.47.1722124aPigRaX&gps-id=7315605&scm=1007.19881.118560.0&scm_id=1007.19881.118560.0&scm-url=1007.19881.118560.0&pvid=b5bfa42f-2292-4789-9970-246ff1d32e97',
+  // tslint:disable-next-line:max-line-length
+  description: 'Men\'s Hip Hop Ribbon Cargo Pants, Men\'s Jogging Pants, Street Fashion 2019, Men\'s Pants with Elastic Waist, Cotton Black Ribbon Pants, W117'
+},
+{
+  name: 'Expanders',
+    id: 10,
+  img: 'https://ae01.alicdn.com/kf/HTB1r1oyhzTpK1RjSZKPq6y3UpXa8/-.jpg',
+  rating: '2/5',
+  // tslint:disable-next-line:max-line-length
+  link: 'https://aliexpress.ru/item/32948555053.html?spm=a2g01.12597576.p99adbb.23.1722124aPigRaX&gps-id=7315995&scm=1007.19881.118560.0&scm_id=1007.19881.118560.0&scm-url=1007.19881.118560.0&pvid=4157890c-3476-420b-86c5-fff0d98bc6c5',
+  // tslint:disable-next-line:max-line-length
+  description: 'Expanders, elastic bands for training, fitness, the gym, rubber loops, latex, for yoga, the gym, strength training, sports elastic bands'
+}
+];
