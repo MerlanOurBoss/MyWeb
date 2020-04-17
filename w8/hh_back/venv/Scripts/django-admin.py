3eb1f2b0d29f3806b9a50003bd8a2b5e35d8ad16
Merlan <merlan01tel@gmail.com>
2020-04-17 14:50:59 +0600

    deleted

--- w8/hh_back/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Merlan\Desktop\LABS\HMTL\Labs\w8\hh_back\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
